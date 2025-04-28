14f8051ad1f1e15bb526fe7fa3f9adcc36e66268
ridwan <ridwanullahgbolagade@gmail.com>
2025-04-28 12:08:26 +0100

    add

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.listen(3002, ()=>{
+    console.log("linstening");
+})
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).send("welcome to port 3002")
+})
